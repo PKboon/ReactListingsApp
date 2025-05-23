90d8a1750eedb7959e8a85627af81dd5d2268f07
Pikulkaew Boonpeng <51871643+PKboon@users.noreply.github.com>
2025-05-22 20:42:44 -0400

    complete module 2
    - practice useState
    - practice passing values upward with a callback prop
    - enable filters (date, number of guests, and name)

---
+++ src/components/ListingFilters.jsx
@@ -0,0 +1,38 @@
+import { Search } from 'lucide-react';
+import { useState } from 'react';
+
+import { Button, DateRangePicker, Input, Stepper } from '@/components/ui';
+
+const ListingFilters = ({ onClick }) => {
+  const today = new Date();
+  const [dates, setDates] = useState();
+  const [guests, setGuests] = useState(2);
+  const [search, setSearch] = useState('');
+
+  const handleClick = () => {
+    onClick({ dates, guests, search });
+  };
+
+  return (
+    <div className='flex flex-row items-center justify-center gap-2'>
+      <Input
+        className='w-[400px]'
+        placeholder='Search destinations'
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <DateRangePicker
+        minDate={today}
+        placeholder='Add dates'
+        value={dates}
+        onChange={setDates}
+      />
+      <Stepper label='guest' value={guests} onChange={setGuests} />
+      <Button onClick={handleClick}>
+        <Search className='h-4 w-4' />
+      </Button>
+    </div>
+  );
+};
+
+export default ListingFilters;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,47 @@
-import { listings } from '@/api/data/listings';
+import { useState } from 'react';
+
+import {
+  isListingAvailable,
+  listings as staticListings,
+} from '@/api/data/listings';
+import ListingFilters from '@/components/ListingFilters';
 import ListingList from '@/components/ListingList';
+import { Separator } from '@/components/ui';
 
 const HomePage = () => {
+  const [listings, setListings] = useState(staticListings);
+
+  const handleFilters = (filters) => {
+    let filteredListings = staticListings;
+    const { dates, guests, search } = filters;
+
+    if (dates) {
+      filteredListings = filteredListings.filter((listing) =>
+        isListingAvailable(listing, dates),
+      );
+    }
+
+    if (guests) {
+      filteredListings = filteredListings.filter(
+        (listing) => listing.maxGuests >= guests,
+      );
+    }
+
+    if (search) {
+      filteredListings = filteredListings.filter((listing) =>
+        listing.name.toLowerCase().includes(search.toLowerCase()),
+      );
+    }
+
+    setListings(filteredListings);
+  };
+
   return (
     <div className='container py-4'>
+      <div className='mb-4'>
+        <ListingFilters onClick={handleFilters} />
+        <Separator className='my-4' />
+      </div>
       <ListingList listings={listings} />
     </div>
   );
