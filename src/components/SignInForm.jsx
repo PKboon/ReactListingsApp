b4314d346145854f1a1eb50d4aa636030c1b3124
Pikulkaew Boonpeng <51871643+PKboon@users.noreply.github.com>
2025-05-31 14:49:43 -0400

    refactor form and input

--- src/components/SignInForm.jsx
+++ src/components/SignInForm.jsx
@@ -4,12 +4,13 @@ import * as z from 'zod';
 
 import api from '@/api';
 import { useAuth } from '@/components/AuthProvider';
+import Form from '@/components/Form';
+import TextInput from '@/components/TextInput';
 import {
   Button,
   Card,
   CardContent,
   CardHeader,
-  Input,
   Separator,
 } from '@/components/ui';
 
@@ -21,12 +22,7 @@ const signInFormSchema = z.object({
 const SignInForm = () => {
   const { setToken } = useAuth();
 
-  const {
-    formState: { errors, isSubmitting },
-    handleSubmit,
-    register,
-    setError,
-  } = useForm({
+  const form = useForm({
     resolver: zodResolver(signInFormSchema),
   });
 
@@ -35,7 +31,7 @@ const SignInForm = () => {
       const response = await api.post('/api/signin', data);
       setToken(response.data.accessToken);
     } catch (e) {
-      setError('root', {
+      form.setError('root', {
         message: e.response.data.message,
       });
     }
@@ -51,35 +47,22 @@ const SignInForm = () => {
         <Separator />
       </CardHeader>
       <CardContent>
-        <form className='flex flex-col gap-4'>
-          <div>
-            <Input {...register('email')} placeholder='name@example.com' />
-            {errors['email'] && (
-              <div className='mt-2 text-sm text-red-500'>
-                {errors['email'].message}
-              </div>
-            )}
-          </div>
+        <Form form={form}>
+          <TextInput
+            control={form.control}
+            name='email'
+            placeholder='name@example.com'
+          />
 
-          <div>
-            <Input {...register('password')} type='password' />
-            {errors['password'] && (
-              <div className='mt-2 text-sm text-red-500'>
-                {errors['password'].message}
-              </div>
-            )}
-          </div>
+          <TextInput control={form.control} name='password' type='password' />
 
-          <Button disabled={isSubmitting} onClick={handleSubmit(onSubmit)}>
-            {isSubmitting ? 'Loading...' : 'Sign In'}
+          <Button
+            disabled={form.formState.isSubmitting}
+            onClick={form.handleSubmit(onSubmit)}
+          >
+            {form.formState.isSubmitting ? 'Loading...' : 'Sign In'}
           </Button>
-
-          {errors.root && (
-            <div className='text-center text-sm text-red-500'>
-              {errors.root.message}
-            </div>
-          )}
-        </form>
+        </Form>
       </CardContent>
     </Card>
   );
